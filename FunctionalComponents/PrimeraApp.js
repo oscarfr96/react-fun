5c8c270da17ab9c8255f957bf7ab65c8619fd114
Óscar Fraile <81364603+oscarfr96@users.noreply.github.com>
2021-12-05 20:17:02 +0100

    Update PrimeraApp.js

--- FunctionalComponents/PrimeraApp.js
+++ FunctionalComponents/PrimeraApp.js
@@ -1,11 +1,28 @@
 //Importamos React
-import React from 'react';
+import React, { Fragment } from 'react';
+//Importamos los propTypes
+import PropTypes from 'prop-types';
 
 // Functional Component
-const PrimeraApp = () =>{
+const PrimeraApp = ( {saludo, subtitulo} ) =>{
 
+    return (
+        //Asi incluimos un Fragment de manera corta
+        <>
+            <h1>{saludo}</h1>
+            {/* <pre>{JSON.stringify(saludo, null, 3)}</pre> */}
+            <p>{subtitulo}</p>
+        </>
+    );
+}
 
-    return <h1>Hola Mundo</h1>
+//Validamos que saludo sea string y que siempre tenga que venir
+PrimeraApp.propTypes={
+    saludo: PropTypes.string.isRequired
+}
+//Valores por defecto para las props en caso de que vengan vacios
+PrimeraApp.defaultProps={
+    subtitulo: "Soy un subtitulo"
 }
 
 export default PrimeraApp;
