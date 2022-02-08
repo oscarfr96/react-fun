9b8956ba9aa7d973a3f66036b79abeae42dd56c3
Óscar Fraile <81364603+oscarfr96@users.noreply.github.com>
2022-02-08 23:24:47 +0100

    Create Expect-toBe.test.js

---
+++ unitTests/Jest/Expect-toBe.test.js
@@ -0,0 +1,12 @@
+test('Los strings debe ser iguales', () => {
+
+    //1. Inicializacion
+    const mensaje = 'Hola Mundo';
+
+    //2. Estimulo
+    const mensaje2 = 'Hola Mundo';
+
+    //3. Observar el comportamiento
+    expect(mensaje).toBe(mensaje2);
+
+})
